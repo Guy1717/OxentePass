1edda61252c0f8cfcb472671ff24fbfdf02ae04c
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-11-28 02:12:11 -0300

    feat: criação de novas exceções

---
+++ src/main/java/com/oxentepass/oxentepass/exceptions/PontoVendaInvalidoException.java
@@ -0,0 +1,9 @@
+package com.oxentepass.oxentepass.exceptions;
+
+public class PontoVendaInvalidoException extends RuntimeException {
+    
+    public PontoVendaInvalidoException (String mensagem) {
+        super(mensagem);
+    }
+
+}
