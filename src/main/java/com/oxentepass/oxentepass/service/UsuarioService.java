d6717ac25a13cb5d6d036cb5f4b9c2cba6347662
Guilherme Paes <guy.paes176@gmail.com>
2025-12-06 15:41:54 -0300

    feat: cria interface de UsuarioService
    define métodos base para o UsuarioService

---
+++ src/main/java/com/oxentepass/oxentepass/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.oxentepass.oxentepass.service;
+
+import com.oxentepass.oxentepass.entity.Usuario;
+
+public interface UsuarioService {
+
+    public void cadastrarUsuario(Usuario usuario);
+
+    public void autenticarUsuario(String email, String senha);
+
+    public void atualizarDadosUsuario(Long id, Usuario dados);
+
+    public void atualizarSenhaUsuario(Long id, String senha);
+
+}
