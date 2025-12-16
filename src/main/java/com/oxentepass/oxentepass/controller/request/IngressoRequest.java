f780f6531f66c78e087cde8270d5ebdcf21d892d
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-12-15 23:21:35 -0300

    fix(#17): adicionando verificaçoes em IngressoRequest

--- src/main/java/com/oxentepass/oxentepass/controller/request/IngressoRequest.java
+++ src/main/java/com/oxentepass/oxentepass/controller/request/IngressoRequest.java
@@ -4,31 +4,35 @@ import java.math.BigDecimal;
 
 import com.oxentepass.oxentepass.entity.Ingresso;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record IngressoRequest(
 
     @NotBlank(message = "O campo \"tipoIngresso\" é obrigatório.")
     String tipoIngresso,
+
     @NotBlank(message = "O campo \"valorBase\" é obrigatório.")
+    @Min(value = 0, message = "O valor base do ingresso deve ser positivo.")
     BigDecimal valorBase,
-    @NotBlank(message = "O campo \"quantidadeDisponivel\" é obrigatório.") 
+
+    @NotBlank(message = "O campo \"quantidadeDisponivel\" é obrigatório.")
+    @Min(value = 1, message = "A quantidade deve ser, no mínimo, uma unidade.")
     int quantidadeDisponivel,
+
     @NotBlank(message = "O campo \"temMeiaEntrada\" é obrigatório.")
     boolean temMeiaEntrada
 
 ) {
-
     public Ingresso paraEntidade() {
-
-    Ingresso ingresso = new Ingresso();
+        Ingresso ingresso = new Ingresso();
 
         ingresso.setTipo(tipoIngresso);
         ingresso.setValorBase(valorBase);
         ingresso.setQuantidadeDisponivel(quantidadeDisponivel);
         ingresso.setTemMeiaEntrada(temMeiaEntrada);
+        
         return ingresso;
-
     }
 }
     
