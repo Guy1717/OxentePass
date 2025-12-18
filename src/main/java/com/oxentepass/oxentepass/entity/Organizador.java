883bb6224d0fa84879f404869bc55dd45034f023
Guilherme Paes <guy.paes176@gmail.com>
2025-12-18 02:49:53 -0300

    feat(#30): cria classe Organizador (COM AGREGAÇÃO)

--- src/main/java/com/oxentepass/oxentepass/entity/Organizador.java
+++ src/main/java/com/oxentepass/oxentepass/entity/Organizador.java
@@ -1,8 +1,25 @@
 package com.oxentepass.oxentepass.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
 
 @Entity
-public class Organizador extends Usuario {
-    
+@Data
+public class Organizador {
+    @Id
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    private String cnpj;
+    private String telefone;
+    private String biografia;
+    private double notaReputacao;
 }
--- src/main/java/com/oxentepass/oxentepass/entity/Usuario.java
+++ src/main/java/com/oxentepass/oxentepass/entity/Usuario.java
@@ -3,19 +3,13 @@ package com.oxentepass.oxentepass.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Setter;
 
 @Entity
 @Data
-@Inheritance(strategy = InheritanceType.JOINED)
 public class Usuario {
     @Id
     @GeneratedValue
-    @Setter(AccessLevel.NONE)
     private long id;
     private String nome;
     private String cpf;
