7969e18baffa622e40d232cc967b7dceaac743d5
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-12-18 21:53:52 -0300

    feat: adiçao e modificaçao de exceçoes

--- src/main/java/com/oxentepass/oxentepass/exceptions/ArquivoInvalidoException.java
+++ src/main/java/com/oxentepass/oxentepass/exceptions/ArquivoInvalidoException.java
@@ -3,7 +3,8 @@ package com.oxentepass.oxentepass.exceptions;
 public class ArquivoInvalidoException extends RuntimeException {
     
     //Cod. HTTP: 400
-    //Envio de arquivo (imagem) inválido 
+    //Envio de arquivo (imagem) inválido
+    //Por razões de: arquivo vazio e tamanho acima do esperado
     public ArquivoInvalidoException (String mensagem) {
         super(mensagem);
     }
