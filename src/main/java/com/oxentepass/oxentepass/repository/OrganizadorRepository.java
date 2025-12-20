31cb0c8cb6ec2bc3626b6d9a76728f46ff969170
Guilherme Paes <guy.paes176@gmail.com>
2025-12-20 02:56:48 -0300

    fix(#35): remove redundância do OrganizadorRepository

--- src/main/java/com/oxentepass/oxentepass/repository/OrganizadorRepository.java
+++ src/main/java/com/oxentepass/oxentepass/repository/OrganizadorRepository.java
@@ -1,6 +1,5 @@
 package com.oxentepass.oxentepass.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +9,5 @@ import com.oxentepass.oxentepass.entity.Organizador;
 @Repository
 public interface OrganizadorRepository extends JpaRepository<Organizador, Long> {
 
-    public List<Organizador> findAll();
 
 }
