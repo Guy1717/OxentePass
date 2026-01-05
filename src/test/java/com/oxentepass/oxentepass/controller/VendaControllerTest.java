92d0a35325e54e6dc6af0ab27f24780af2d3118d
Arthur de Sá Tenório <arthursa147@gmail.com>
2026-01-04 23:44:30 -0300

    test: corrigindo VendaControllerTest

--- src/test/java/com/oxentepass/oxentepass/controller/VendaControllerTest.java
+++ src/test/java/com/oxentepass/oxentepass/controller/VendaControllerTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import jakarta.transaction.Transactional;
+import com.oxentepass.oxentepass.service.VendaService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,11 +17,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(VendaController.class)
 @ActiveProfiles("test")
-@Transactional
 class VendaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private VendaService vendaService;
 
     @Test
     void deveFinalizarVenda() throws Exception {
